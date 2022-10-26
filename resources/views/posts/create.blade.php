411e503ff8b38892867cbb9b19fbe7ace9ef1df4
OhishiYukito <yukito.oishi@outlook.jp>
2022-10-26 17:11:11 +0900

    curriculum08-4_work4

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -9,12 +9,14 @@
             @csrf
             <div class="title">
                 <label>タイトル:
-                    <input type="text" name="post[title]" size="10" />
+                    <input type="text" name="post[title]" size="10" value="{{old('post.title')}}" />
+                    <p class='title_error' style='color:red'>{{$errors->first('post.title')}}</p>
                 </label>
             </div>
             <div class="content">
                 <label for='content'>本文:</label><br>
-                <textarea id='content' name="post[body]" placeholder="ここが本文"></textarea>
+                <textarea id='content' name="post[body]" placeholder="ここが本文" value="{{old('post.body')}}"></textarea>
+                <p class='title_error' style='color:red'>{{$errors->first('post.body')}}</p>
             </div>
                 <input type="submit" value="保存" />
         </form>
