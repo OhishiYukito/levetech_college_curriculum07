ae1a7192c22877addbce3179ffcfde7e1581b5b4
OhishiYukito <yukito.oishi@outlook.jp>
2022-10-26 18:14:26 +0900

    curriculum08-5_work2

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>create</title>
+    </head>
+    <body>
+        <h1>編集</h1>
+        <form action="/posts/{{$post->id}}" method="POST">
+            @csrf
+            @method('PUT')
+            <div class="title">
+                <label>タイトル:
+                    <input type="text" name="post[title]" size="10" value="{{$post->title}}" />
+                    <p class='title_error' style='color:red'>{{$errors->first('post.title')}}</p>
+                </label>
+            </div>
+            <div class="content">
+                <label for='content'>本文:</label><br>
+                <textarea id='content' name="post[body]" >{{$post->body}}</textarea>
+                <p class='title_error' style='color:red'>{{$errors->first('post.body')}}</p>
+            </div>
+                <input type="submit" value="保存" />
+        </form>
+        <div class="footer">
+            <a href="/">投稿一覧へ</a>
+        </div>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ use App\Http\Controllers\PostController;
  */
 Route::get('/posts/create', [PostController::class, 'create']);
 
+Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
+
 Route::get('/posts/{post}', [PostController::class, 'show']);
 
 Route::get('/', [PostController::class, 'index']);
